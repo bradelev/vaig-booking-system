fc8aa2fffb8c6a0a19cdc8b89c7504bea5bddacf
Braulio De Leon <hi@brauliodeleon.com>
2026-03-18 01:32:17 -0300

    fix(db): update db permissions (#44)

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -1,9 +1,10 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types";
 
-export function createClient() {
-  return createBrowserClient<Database>(
+export function createAdminClient() {
+  return createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
 }
+
